5604eec426744cc060f94d9e9b33a189b1215ea0
Fatmanur Çetintaş <99668549+fatmanur7@users.noreply.github.com>
2023-02-28 23:36:25 +0300

    Add files via upload

---
+++ c02/ex01/ft_strncpy.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/09 18:55:27 by facetint          #+#    #+#             */
+/*   Updated: 2023/02/09 18:56:31 by facetint         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ c02/ex09/ft_strcapitalize.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: facetint <facetint@student.42kocaeli.com.  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/05 04:35:10 by facetint          #+#    #+#             */
+/*   Updated: 2023/02/10 17:04:45 by facetint         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	ft_strlowcase(str);
+	while (str[i])
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if (j == 1)
+				str[i] -= 32;
+			j = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
+	}
+	return (str);
+}
